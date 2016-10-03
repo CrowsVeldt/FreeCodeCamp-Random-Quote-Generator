de46369670eaf998b0a8623b7f4d53d2381ac780
Crows_Veldt <Snorri.Jonas@gmail.com>
2016-10-03 16:28:19 +0300

    Inital Commit

---
+++ Random Quote.js
@@ -0,0 +1,24 @@
+$( document ).ready()
+
+$.ajaxSetup({ cache: false });
+
+$(".quote-button").click(function(){
+  $("p").fadeOut("slow");
+
+  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+    $(".quote").html('"' + a[0].content + '"')
+    $(".author").html('<b> - '+ a[0].title + ' </b>')
+
+});
+  
+  $("p").fadeIn();
+
+});
+
+$(".twitter-button").click(function(){
+  var quote = $(".quote").text();
+  var author = $(".author").text();
+  var url = "https://twitter.com/intent/tweet?text=" + quote + " --" + author;
+
+  window.open(url, '_blank');
+  });
